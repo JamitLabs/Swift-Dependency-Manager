1da6b950d3238e27adf54d766353a6615074d8d4
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-10-25 12:36:55 +0200

    Revert "Integrate SwiftPM as library + load manifest file"
    This reverts commit 28175310bec0a53a29f4d1273d3100f35065b743.

--- Package.swift
+++ Package.swift
@@ -12,9 +12,8 @@ let package = Package(
         .package(url: "https://github.com/kiliankoe/CLISpinner.git", .upToNextMinor(from: "0.3.5")),
         .package(url: "https://github.com/Flinesoft/HandySwift.git", .upToNextMajor(from: "2.6.0")),
         .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.1.4")),
-        .package(url: "https://github.com/jakeheis/SwiftCLI.git", .upToNextMajor(from: "5.1.2")),
-        .package(url: "https://github.com/JamitLabs/swift-package-manager.git", .branch("swift-4.2-branch")),
-        .package(url: "https://github.com/tuist/xcodeproj.git",  .upToNextMajor(from: "6.1.0"))
+        .package(url: "https://github.com/jakeheis/SwiftCLI", .upToNextMajor(from: "5.1.2")),
+        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.1.0"))
     ],
     targets: [
         .target(
@@ -28,7 +27,6 @@ let package = Package(
                 "HandySwift",
                 "Rainbow",
                 "SwiftCLI",
-                "SwiftPM",
                 "xcodeproj"
             ]
         ),
--- Sources/SwiftDependencyManagerKit/Globals/SwiftPM.swift
+++ Sources/SwiftDependencyManagerKit/Globals/SwiftPM.swift
@@ -1,33 +1,14 @@
-import Basic
 import Foundation
-import PackageLoading
-import PackageModel
-import Utility
-
-struct ManifestResource: ManifestResourceProvider {
-    let swiftCompiler = AbsolutePath("/Users/Arbeit/.swiftenv/shims/swiftc")
-    let libDir = AbsolutePath("/usr/local/lib/")
-}
+import SwiftCLI
 
 struct SwiftPM {
     static let shared = SwiftPM()
 
     func updateDependencies() throws {
-        let manifest = try loadManifest()
-        // TODO: not yet implemented
+        try run(bash: "swift package update")
     }
 
     func resolveDependencies() throws {
-        let manifest = try loadManifest()
-        // TODO: not yet implemented
-    }
-
-    private func loadManifest() throws -> Manifest {
-        let manifestLoader = ManifestLoader(resources: ManifestResource(), isManifestSandboxEnabled: true)
-        return try manifestLoader.load(
-            package: AbsolutePath(FileManager.default.currentDirectoryPath),
-            baseURL: FileManager.default.currentDirectoryPath,
-            manifestVersion: .v4_2
-        )
+        try run(bash: "swift package resolve")
     }
 }
